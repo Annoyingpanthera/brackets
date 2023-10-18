913d22174aad28948249252f0c2ec6eb914578de
Annoyingpanthera <122933423+Annoyingpanthera@users.noreply.github.com>
2023-10-18 11:58:29 +0200

    init:end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketMap = new Map(bracketsConfig.map(pair => pair.reverse()));
+
+  for (let char of str) {
+    if (bracketMap.has(char)) {
+      const matchingBracket = bracketMap.get(char);
+
+      // If the opening and closing brackets are the same, pop the stack if the last bracket is the same.
+      if (matchingBracket[0] === matchingBracket[1] && stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else if (stack.length > 0 && stack[stack.length - 1] === matchingBracket[0]) {
+        stack.pop(); // Pop the matching opening bracket from the stack.
+      } else {
+        stack.push(char); // Push the opening bracket onto the stack.
+      }
+    } else {
+      stack.push(char); // Push non-bracket characters onto the stack.
+    }
+  }
+
+  return stack.length === 0;
 }
+
+
